1eb05320486c8588bac9099fe6dce1ecfa42f473
Daniel López puig <danielopez92@gmail.com>
2020-05-04 19:46:13 +0200

    finish project 5

---
+++ sentiment-analysis-network/object_info.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+def print_info(obj_info):
+    print("var: " + str(obj_info))
+    print("shape: " + str(obj_info.shape))
+    print("type: " + str(type(obj_info)))
+
+
+
+# X = np.array([1,2,3])
+# Y = np.array([1,2])
+# print_info(X)
+# print("---")
+# print_info(X[:,None])
+# print("---")
+# print_info(Y*X[:,None])
+
